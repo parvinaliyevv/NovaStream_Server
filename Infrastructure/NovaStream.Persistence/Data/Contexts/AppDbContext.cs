c663b2dcea64bd80980d240b3b9627fdd7cd560d
parvinaliyevv <aliyevv_p@outlook.com>
2023-01-27 00:57:27 +0400

    architecture

---
+++ Infrastructure/NovaStream.Persistence/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace NovaStream.Persistence.Data.Contexts;
+
+public class AppDbContext: DbContext
+{
+
+
+	public AppDbContext(DbContextOptions options): base(options) { }
+
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		
+	}
+}
